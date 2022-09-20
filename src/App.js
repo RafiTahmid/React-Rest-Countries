1f8bcc305cc5cbce8a0f551445d7e9760019d2e0
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-21 00:07:46 +0600

    Country Displayed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 function LoadCountries(){
   const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
+  useEffect( () => {
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then(data => setCountries(data))
@@ -23,8 +23,20 @@ function LoadCountries(){
     <div>
       <h1>Visiting every country of the world..!!</h1>
       <h2>Available Countries: {countries.length}</h2>
+
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
     </div>
   )
 }
 
+function Country(props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+    </div>
+  )
+}
 export default App;
